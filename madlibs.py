906aa53c960e27128488de057b3dbfe2499a1746
muhammadh <muhammad.hamza@xflowresearch.com>
2021-11-18 11:56:51 +0500

    first beginner code for madlibs

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+#string Concatenation :p
+
+# youtuber = "Hamza"
+
+# print("subscribe to {}".format(youtuber))
+# print("subscribe to " + youtuber)
+# print(f"subscribe to {youtuber}")
+
+#--------------------------------------------------------------
+
+#Madlibs understandings :)
+
+# adj = input("Adjective: ")
+# verb1 = input("verb1: ")
+# verb2 = input("verb2: ")
+# famous_person = input("famous person: ")
+
+# madlib = f"computer programming is so {adj}! It makes me so excited all the time becasue \ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+# print (madlib)
---
+++ sample_madlibs/lemon.py
@@ -0,0 +1,6 @@
+def madlib():
+    name = input("name: ")
+
+    madlib = f"my name is {name} and i am emotional!"
+
+    print(madlib)
